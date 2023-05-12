e05732aa6fb4bc7bfd31bbf3da41dac9da494676
esaci <esaci@student.42.fr>
2023-05-12 15:08:48 +0200

    Ajout du compteur ansi que de la modification de la tache 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'toDoList';
+  progress: number = 0;
+  intervalId: any;
+  tache1: string = 'Faire les courses';
+
+  increaseProgress() {
+    if (this.progress === 9) {
+      this.progress = 0;
+      this.tache1 = 'Faire les courses !!';
+    }
+    else {
+      this.progress += 1;
+      this.tache1 = 'Faire les courses';
+    }
+  }
+
+  ngOnInit() {
+    this.intervalId = setInterval(() => {
+      this.increaseProgress();
+    }, 1000);
+  }
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }
